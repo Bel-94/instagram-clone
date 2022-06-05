582c49011b875f431e87e7c1eeaaddc1ea1be9bf
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2022-06-05 19:44:25 +0300

    connect signals file with the app.py file in order to use them

--- instaclone/apps.py
+++ instaclone/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class InstacloneConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'instaclone'
+
+    def ready(self):
+        import instaclone.signals 
