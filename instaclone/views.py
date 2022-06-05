31f13c8d2a963efbe7f7750fade5b63a00790cd1
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2022-06-05 20:43:57 +0300

    add the imports

--- instaclone/views.py
+++ instaclone/views.py
@@ -1,9 +1,31 @@
-
-from django.shortcuts import render, redirect
+from multiprocessing.dummy import current_process
+import threading
+from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
 from django.http import HttpResponse
+
+from . import settings
+from .forms import UpdateUserForm, UpdateProfileForm, AddPostForm
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+from django.template.loader import render_to_string
+from .tokens import account_activation_token
+from django.utils.encoding import force_str
+from django.utils.http import urlsafe_base64_decode
+from .models import Follow, Like, Post, Profile, Comment
+from django.core.mail import EmailMessage
 
 
 # Create your views here.
 
 def welcome(request):
-    return HttpResponse('Welcome to my insta clone')
+    posts = Post.objects.order_by('-date_created').all()
+    profiles = Profile.objects.all()
+    return render('Welcome to my insta clone')
