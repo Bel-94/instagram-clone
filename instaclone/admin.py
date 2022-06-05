119b96d2d237a972112e68f50947876b6550edff
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2022-06-05 18:27:18 +0300

    make migrations

--- instaclone/admin.py
+++ instaclone/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Profile, Post, Like, Follow, Comment
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(Like)
+admin.site.register(Follow)
+admin.site.register(Comment)
