96eeb8e6311489612a71848e29dd973f8729ca80
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2022-06-06 13:25:40 +0300

    deleting the and redoing the authentication

--- Instagram/settings.py
+++ Instagram/settings.py
@@ -155,12 +155,12 @@ MESSAGE_TAGS = {
 }
 
 #Email Setup
-EMAIL_FROM_USER = os.environ.get("EMAIL_FROM_USER")
-EMAIL_BACKEND = os.environ.get("EMAIL_BACKEND")
+# EMAIL_FROM_USER = os.environ.get("EMAIL_FROM_USER")
+# EMAIL_BACKEND = os.environ.get("EMAIL_BACKEND")
 EMAIL_HOST = os.environ.get("EMAIL_HOST")
 EMAIL_PORT = os.environ.get("EMAIL_PORT")
 EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS")
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 
-TESTING = len(sys.argv) > 1 and sys.argv[1] == 'test'
+# TESTING = len(sys.argv) > 1 and sys.argv[1] == 'test'
