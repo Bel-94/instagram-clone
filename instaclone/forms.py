dcd54ddd9f5dbe0028116705fd8dc2a20c9951b8
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2022-06-07 12:54:39 +0300

    edit and update the forms file

--- instaclone/forms.py
+++ instaclone/forms.py
@@ -1,5 +1,32 @@
-from django import forms
+from .models import Image, Profile, Follow, Comment
+from django.forms import ModelForm
 
-class NewsLetterForm(forms.Form):
-    your_name = forms.CharField(label='First Name',max_length=30)
-    email = forms.EmailField(label='Email')
+
+class CreateProfileForm(ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ["created", "account_holder", "followers", "following"]
+
+
+class UploadImageForm(ModelForm):
+    class Meta:
+        model = Image
+        exclude = ["profile", "post_date", "likes"]
+
+
+class EditBioForm(ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ["user"]
+
+
+class FollowForm(ModelForm):
+    class Meta:
+        model = Follow
+        exclude = ["followed", "follower"]
+
+
+class UnfollowForm(ModelForm):
+    class Meta:
+        model = Follow
+        exclude = ["followed", "follower"]
