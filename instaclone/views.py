8e519cc77c1cc3abcc5a4bbfdd466f6c049fba50
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2022-06-07 13:02:08 +0300

    create the email file

--- instaclone/views.py
+++ instaclone/views.py
@@ -1,26 +1,12 @@
-from multiprocessing.dummy import current_process
-import threading
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.contrib import messages
-from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect, Http404
+from .forms import *
+from .models import *
+from .email import send_welcome_email
 
-# from Instagram import settings
-# from .forms import UpdateUserForm, UpdateProfileForm, AddPostForm
-# from django.contrib.auth.forms import PasswordChangeForm
-# from django.contrib.auth import update_session_auth_hash
-# from django.contrib.sites.shortcuts import get_current_site
-# from django.utils.encoding import force_bytes
-# from django.utils.http import urlsafe_base64_encode
-# from django.template.loader import render_to_string
-# # from .tokens import account_activation_token
-# from django.utils.encoding import force_str
-# from django.utils.http import urlsafe_base64_decode
-# from .models import Follow, Like, Post, Profile, Comment
-# from django.core.mail import EmailMessage
 
 
 # Create your views here.
