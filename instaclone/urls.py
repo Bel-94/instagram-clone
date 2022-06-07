bdc5b37fae70cd72516735947029783782ce36a5
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2022-06-07 13:52:13 +0300

    discard all the url paths created before

--- instaclone/urls.py
+++ instaclone/urls.py
@@ -1,25 +1,11 @@
-from django.conf import Settings, settings
-from django.urls import path
+from django.conf import  settings
+from django.urls import path, re_path
 from . import views
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.welcome, name='welcome'),
-    # path('login', views.Login, name="Login"),
-    # path('register', views.Register, name="Register"),
-    path('profile/<str:username>', views.MyProfile, name="MyProfile"),
-    path('user/<str:username>', views.UserProfile, name="UserProfile"),
-    path('profile/<str:username>/edit', views.EditProfile, name="EditProfile"),
-    # path('profile/<str:username>/settings', views.Settings, name="Settings"),
-    path('post/<int:id>', views.SingleImage, name="SingleImage"),
-    path('profile/<str:username>/image/add', views.AddNewPost, name="AddNewPost"),
-    path('post/<int:id>/like', views.PostLike, name="PostLike"),
-    # path('logout', views.Logout, name="Logout"),
-    path('follow/user/<str:username>', views.FollowUser, name="FollowUser"),
-    path('post/<int:id>/comment', views.AddComment, name="AddComment"),
-    path('search-results', views.SearchUsers, name="SearchUsers"),
-    # path('activateuser/<uidb64>/<token>', views.ActivateAccount, name = 'ActivateAccount')
+    
 ]
 
-if Settings:
+if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
